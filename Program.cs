9b4ac1a9543d3d2b026a8cfa1df0c6bbda2f30b6
Ethan Ying Wang <Yingqiu.Wang@mssa.newhorizons.com>
2024-12-03 10:01:58 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Assignment_5._1._2
+{
+    internal class Program
+    {
+        public static int SumOfDigits(int number)
+        {
+            int sum = 0;
+
+            number = Math.Abs(number);
+
+            while (number > 0)
+            {
+                sum += number % 10; 
+                number /= 10;   
+            }
+
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int result = SumOfDigits(num);
+            Console.WriteLine($"The sum of the digits of the number {num} is: {result}");
+        }
+    }
+}
